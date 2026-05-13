6c5af48ca87495594e0ba6ec8d730ab1a07d58b7
Piyush Rajput <piyush123kdif@gmail.com>
2026-05-13 12:34:39 +0530

    amount fix

--- app/Models/UserPayment.php
+++ app/Models/UserPayment.php
@@ -3,15 +3,22 @@
 namespace App\Models;
 
 use MongoDB\Laravel\Eloquent\Model;
+
 class UserPayment extends Model
 {
     protected $connection = 'mongodb';
+
     protected $collection = 'user_payments';
 
     protected $fillable = [
         'user_id',
         'subscription_id',
+
         'status',
+
+        // ✅ add this
+        'amount',
+
         'start_date',
         'end_date'
     ];
